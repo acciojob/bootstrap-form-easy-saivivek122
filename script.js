21b61baa3de22c9900d01bb797743235bc76d14f
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-07-22 13:05:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-
-        document.getElementById("form").addEventListener("submit", function (event) {
-            const form = event.target;
-
-            if (!form.checkValidity()) {
-                event.preventDefault(); // prevent actual form submission
+document.getElementById("form").addEventListener("submit", function (event) {
+const form = event.target;
+if (!form.checkValidity()) {
+event.preventDefault(); 
                 
-            }
+}
 
-            form.classList.add('was-validated');
-        });
+form.classList.add('was-validated');
+});
 
 
